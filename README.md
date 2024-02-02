61accdbb29b45e8b5506c1d184ecc1d601d14487
usheen-h <oisin233@gmail.com>
2024-02-02 10:42:32 +0000

    Final commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Oisin Hogan x18115276
+[devops-lab-2](https://github.com/usheen-h/scripts)
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -14,3 +14,24 @@ touch .gitignore
 echo ".gitignore" >> .gitignore
 echo fol_1 >> .gitignore
 echo fol_2 >> .gitignore
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+chmod 000 fol_1/1_2.txt
+chmod 000 fol_1/1_2.txt
+touch README.md
+echo "# Week 2 Lab – Bash & Git" > README.md
+echo "Oisin Hogan x18115276" > README.md
+echo "[devops-lab-2](https://github.com/usheen-h/scripts)" >> README.md
+echo --- >> README.md
+echo "To run the script.sh run: \`bash script.sh\`" >> README.md
+
+git init
+git add .
+git commit -m "Final commit"
+git remote add origin https://github.com/usheen-h/scripts.git
+git branch -M main
+git push -u origin main
+
+echo Job Completed
