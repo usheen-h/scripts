0c5557e292fcb3798e6143c4a43d863f6d219a92
usheen-h <oisin233@gmail.com>
2024-02-02 10:30:29 +0000

    second

--- script.sh
+++ script.sh
@@ -1,1 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
+mkdir fol_1
+mkdir fol_2
+cd fol_1
+touch 1_1.txt
+touch 1_2.txt
+touch 1_3.txt
+cd ../fol_2
+touch 2_1.txt
+touch 2_2.txt
+touch 2_3.txt
+cd ..
+touch .gitignore
+echo ".gitignore" >> .gitignore
+echo fol_1 >> .gitignore
+echo fol_2 >> .gitignore
